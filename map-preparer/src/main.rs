13b3387475b303e0cc39863048f24697bfec43fc
andreroesti <an.roesti@gmail.com>
2025-02-24 15:08:39 -0800

    start adding code for reading PBF file

--- map-preparer/src/main.rs
+++ map-preparer/src/main.rs
@@ -1,2 +1,38 @@
-fn main() -> () {
+use map_preparer::osm;
+use prost::Message;
+
+const BUFFER_SIZE: usize = 4096;
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    let path = args.get(1).ok_or(std::io::Error::new(std::io::ErrorKind::InvalidInput, "missing input file path command line argument"));
+    let mut file = std::fs::File::open(path?)?;
+    let mut buffer: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+    while let Ok((size, blob_header)) = read_blob_header(&mut file) {
+        println!("Read blob header of size {}.", size);
+    }
+    Ok(())
+}
+
+/// Reads a blob header from the input stream and returns how many bytes total were read plus the blob header.
+fn read_blob_header(in_stream: &mut impl std::io::Read) -> Result<(usize, osm::BlobHeader), OSMPBFParseError> {
+    const SIZE_SIZE: usize = 4;
+    let mut size_buffer: [u8; 4] = [0; 4];
+    std::io::Read::read(in_stream, &mut size_buffer).map_err(|e| { OSMPBFParseError::new(e) } )?;
+    let size: usize = u32::from_be_bytes(size_buffer).try_into().map_err(|e| { OSMPBFParseError::new(e) })?;
+    let mut blob_header_buffer = Vec::<u8>::with_capacity(size);
+    //let header = osm::HeaderBlock::decode(&buffer[..])?;
+    let blob_header = osm::BlobHeader::decode(&blob_header_buffer as &[u8]).map_err(|e| { OSMPBFParseError::new(e) })?;
+    Ok((size, blob_header))
+}
+
+struct OSMPBFParseError {
+    root_cause: Option<Box<dyn std::error::Error>>
+}
+
+impl OSMPBFParseError {
+    fn new(root_cause: impl std::error::Error + 'static) -> Self { // the + 'static here constrains the lifetimes of any potential references contained within the error
+        Self {
+            root_cause: Some(Box::new(root_cause) as Box<dyn std::error::Error>)
+        }
+    }
 }
